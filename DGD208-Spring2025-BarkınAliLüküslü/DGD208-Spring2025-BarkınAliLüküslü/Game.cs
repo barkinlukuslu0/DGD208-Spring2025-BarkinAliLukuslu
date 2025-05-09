01b11581761e2a94ecf6409058c9bc00455d92cf
Barkın Ali Lüküslü <barkinlukuslumain@gmail.com>
2025-05-09 20:08:02 +0300

    Second Commit
    I added quitting game and credits

--- DGD208-Spring2025-BarkınAliLüküslü/DGD208-Spring2025-BarkınAliLüküslü/Game.cs
+++ DGD208-Spring2025-BarkınAliLüküslü/DGD208-Spring2025-BarkınAliLüküslü/Game.cs
@@ -33,13 +33,35 @@ namespace DGD208_Spring2025_BarkýnAliLüküslü
         private string GetUserInput()
         {
             // Use this to display appropriate menu and get user inputs
-            return "";
+
+            Console.Clear();
+            Console.WriteLine("1. View Pets");
+            Console.WriteLine("2. Use Item on Pet");
+            Console.WriteLine("9. Credits");
+            Console.WriteLine("0. Exit");
+            Console.Write("Your choice: ");
+            return Console.ReadLine();
         }
 
         private async Task ProcessUserChoice(string choice)
         {
             // Use this to process any choice user makes
             // Set _isRunning = false to exit the game
+
+            if (choice == "0")
+            {
+                _isRunning = false;
+            }
+            else if (choice == "9")
+            {
+                // Display credits
+                Console.WriteLine("This game created by Barkýn Ali Lüküslü");
+                Console.WriteLine("Press any key to return...");
+                Console.ReadKey();
+
+                //if user press any key the game will continue
+                GetUserInput();
+            }
         }
     }
 }
